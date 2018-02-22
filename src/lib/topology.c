960c5c2969ae19b14bc9c7b9c8377b68c7125faa
Ikey Doherty <ikey@solus-project.com>
2018-02-22 15:25:21 +0000

    Begin splitting out boot-adjust from the common library
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/lib/topology.c
@@ -0,0 +1,35 @@
+/*
+ * This file is part of yokeybob.
+ *
+ * Copyright © 2018 Solus Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include "topology.h"
+
+/**
+ * TODO: Actually init.
+ */
+bool yb_topology_init(YbTopology *self)
+{
+        return false;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/lib/topology.h
@@ -0,0 +1,52 @@
+/*
+ * This file is part of yokeybob.
+ *
+ * Copyright © 2018 Solus Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+#define _GNU_SOURCE
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+ * YbTopology is a simple composite struct to query various aspects of the
+ * system in order to provide more information to act on.
+ */
+typedef struct YbTopology {
+        /* Memory information */
+        struct {
+                int64_t mem_mib; /**<Memory available in MiB */
+                bool swap_avail; /**<If swap is enabled */
+        } memory;
+
+        /* Root partition information (currently unused) */
+        struct {
+                int __reserved1;
+        } rootfs;
+} YbTopology;
+
+/**
+ * Initialise a YbTopology struct to analyse the system.
+ */
+bool yb_topology_init(YbTopology *top);
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
